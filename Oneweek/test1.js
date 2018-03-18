47c6dffcbcd79083fa44ffb0fd60133645481041
changchanglccc <lichong06091992@gmail.com>
2018-03-18 10:27:06 -0400

    function done

--- Oneweek/test1.js
+++ Oneweek/test1.js
@@ -657,3 +657,254 @@ arr.forEach(function(chr) { //然后再直接调用数组的forEach方法。
 */
 
 /** 函数 Function */
+//函数声明：1） function命令 2）函数表达式 3）Function构造函数
+/*
+1）function print(s) {
+    console.log(s);
+  }
+2）var print = function(s) {
+    console.log(s);
+  };
+3）var add = new Function(       //声明函数的方式不直观 ， 几乎无人使用
+    'x',
+    'y',
+    'return x + y'
+  );
+  
+  // 等同于
+  function add(x, y) {
+    return x + y;
+  }
+// Function构造函数接受三个参数，除了最后一个参数是add函数的“函数体”，其他参数都是add函数的参数。
+// 你可以传递任意数量的参数给Function构造函数，只有最后一个参数会被当做函数体，如果只有一个参数，该参数就是函数体。
+
+// 采用函数表达式声明函数时，function命令后面不带有函数名。如果加上函数名，该函数名只在函数体内部有效，在函数体外部无效。
+var print = function x(){
+  console.log(typeof x);
+};
+x       // ReferenceError: x is not defined
+print()         // function
+
+上面代码在函数表达式中，加入了函数名x。这个x只在函数体内部可用，指代函数表达式本身，其他地方都不可用。这种写法的用处有两个，一是可以在函数体内部调用自身，二是方便除错（除错工具显示函数调用栈时，将显示函数名，而不再显示这里是一个匿名函数）。因此，下面的形式声明函数也非常常见。
+
+var f = function f() {};
+需要注意的是，函数的表达式需要在语句的结尾加上分号，表示语句结束。而函数的声明在结尾的大括号后面不用加分号。总的来说，这两种声明函数的方式，差别很细微，可以近似认为是等价的。
+
+var print = function (s){
+    console.log(s);
+};
+print('haha');
+*/
+// 如果同一个函数被多次声明，后面的声明就会覆盖前面的声明。
+
+/*
+JavaScript 引擎将函数名视同变量名，所以采用function命令声明函数时，整个函数会像变量声明一样，被提升到代码头部。所以，下面的代码不会报错。
+f();
+function f() {}
+但是，如果采用赋值语句定义函数，JavaScript 就会报错。
+var f;
+f();
+f = function () {};
+上面代码第二行，调用f的时候，f只是被声明了，还没有被赋值，等于undefined，所以会报错。
+因此，如果同时采用function命令和赋值语句声明同一个函数，最后总是采用赋值语句的定义。
+*/
+
+//不能在条件语句中声明函数
+// 要达到在条件语句中定义函数的目的，只有使用函数表达式。
+
+/**
+ * 函数的属性和方法
+ * 
+ * name属性 + length属性（返回函数参数的个数） 
+ * 
+ * toString()方法： 返回一个字符串，内容是函数的源码
+ */
+
+ /**
+  * 函数作用域
+
+//   函数内部定义的变量，会在该作用域内覆盖同名全局变量。
+//   注意，对于var命令来说，局部变量只能在函数内部声明，在其他区块中(比如 条件判断语句中)声明，一律都是全局变量。
+
+// var命令声明的变量，不管在什么位置，变量声明都会被提升到函数体的头部。
+
+// 函数本身作用域： 函数执行时所在的作用域，是定义时的作用域，而不是调用时所在的作用域。
+
+// 很容易犯错的一点是，如果函数A调用函数B，却没考虑到函数B不会引用函数A的内部变量。
+var x = function () {
+  console.log(a);
+};
+
+function y(f) {
+  var a = 2;
+  f();
+}
+
+y(x)
+// ReferenceError: a is not defined
+上面代码将函数x作为参数，传入函数y。但是，函数x是在函数y体外声明的，作用域绑定外层，因此找不到函数y的内部变量a，导致报错。
+ */
+
+ /**
+  * 参数
+
+//  需要注意的是，函数的length属性与实际传入的参数个数无关，只反映函数预期传入的参数个数。
+//  但是，没有办法只省略靠前的参数，而保留靠后的参数。如果一定要省略靠前的参数，只有显式传入undefined。
+
+// 传递方式：
+    函数参数如果是原始类型的值（数值、字符串、布尔值），传递方式是传值传递（passes by value）。
+    这意味着，在函数体内修改参数值，不会影响到函数外部。
+
+    但是，如果函数参数是复合类型的值（数组、对象、其他函数），传递方式是传址传递（pass by reference）。
+    也就是说，传入函数的原始值的地址，因此在函数内部修改参数，将会影响到原始值。
+
+    注意，如果函数内部修改的，不是参数对象的某个属性，而是替换掉整个参数，这时不会影响到原始值。
+
+var obj = [1, 2, 3];
+function f(o) {
+  o = [2, 3, 4];
+}
+f(obj);
+
+obj         // [1, 2, 3]、
+    在函数f内部，参数对象obj被整个替换成另一个值。这时不会影响到原始值。这是因为，形式参数（o）的值实际是参数obj的地址，重新对o赋值导致o指向另一个地址，保存在原地址上的值当然不受影响。
+
+
+    // 有同名参数，取最后出现的那个的值 eg： function f(a,a){...}
+
+    //调用函数f的时候，没有提供第二个参数，a的取值就变成了undefined。这时，如果要获得第一个a的值，可以使用arguments对象。
+function f(a, a) {
+  console.log(arguments[0]);
+}
+
+f(1) // 1
+
+// argument 对象
+可以在函数体内部读取所有参数。这就是arguments对象的由来。
+
+var f = function(a, b) {
+  'use strict'; // 开启严格模式
+  arguments[0] = 3; // 无效
+  arguments[1] = 2; // 无效
+  return a + b;
+}
+
+f(1, 1) // 2
+上面代码中，函数体内是严格模式，这时修改arguments对象就是无效的。
+
+// 通过arguments对象的length属性，可以判断函数调用时到底带几个参数。
+
+// 函数的其他知识点
+// 闭包！closure
+
+function f1() {
+  var n = 999;
+  function f2() {
+    console.log(n);
+  }
+  return f2;
+}
+
+var result = f1();
+result(); // 999
+
+闭包就是函数f2，即能够读取其他函数内部变量的函数。由于在 JavaScript 语言中，只有函数内部的子函数才能读取内部变量，因此可以把闭包简单理解成“定义在一个函数内部的函数”
+闭包最大的特点，就是它可以“记住”诞生的环境，比如f2记住了它诞生的环境f1，所以从f2可以得到f1的内部变量。在本质上，闭包就是将函数内部和函数外部连接起来的一座桥梁。
+
+// 链式作用域
+function f1(){
+    var n = 999;
+    function f2(){      //这时 f1内的所有局部变量，对f2 都是可见的
+        // console.log(n);
+        return n
+    };
+    return f2;
+}
+
+var result = f1();
+// result();   //999
+console.log(result());
+//在本质上，闭包就是将函数内部和函数外部连接起来的一座桥梁。
+
+// 闭包的最大用处有两个，一个是可以读取函数内部的变量，另一个就是让这些变量始终保持在内存中，即闭包可以使得它诞生环境一直存在。
+// 请看下面的例子，闭包使得内部变量记住上一次调用时的运算结果。
+
+function createIncrementor(start) {
+    return function () {
+      return start++;
+    };
+  }
+  
+  var inc = createIncrementor(5);
+  inc() // 5
+  inc() // 6
+  inc() // 7
+// 上面代码中，start是函数createIncrementor的内部变量。通过闭包，start的状态被保留了，每一次调用都是在上一次调用的基础上进行计算。从中可以看到，闭包inc使得函数createIncrementor的内部环境，一直存在。
+// 所以，闭包可以看作是函数内部作用域的一个接口。
+// 为什么会这样呢？原因就在于inc始终在内存中，而inc的存在依赖于createIncrementor，因此也始终在内存中，不会在调用结束后，被垃圾回收机制回收。
+
+// 闭包的另一个用处，是封装对象的私有属性和私有方法。
+function Person(name){
+    var _age;
+    function setAge(n){
+        _age = n;
+    };
+    function getAge(){
+        return _age;
+    };
+    return{
+        name: name,
+        getAge: getAge,
+        setAge: setAge
+    };
+}
+
+var p1 = Person('Bob');
+p1.setAge(25);
+console.log(p1.getAge());
+注意，外层函数每次运行，都会生成一个新的闭包，而这个闭包又会保留外层函数的内部变量，所以内存消耗很大。因此不能滥用闭包，否则会造成网页的性能问题。
+*/
+
+/**
+ * Immediately-invoked function expression 立即调用函数表达式
+ */
+
+ /*
+(function(){ ... }());
+// 或者
+(function(){ ...})();   // 最后的分号都是必须的
+
+// 上面两种写法都是以圆括号开头，引擎就会认为后面跟的是一个表示式，而不是函数定义语句，所以就避免了错误。
+// 这就叫做“立即调用的函数表达式”（Immediately-Invoked Function Expression），简称 IIFE。
+
+// 通常情况下，只对匿名函数使用这种“立即执行的函数表达式”。
+// 它的目的有两个：一是不必为函数命名，避免了污染全局变量；二是 IIFE 内部形成了一个单独的作用域，可以封装一些外部无法读取的私有变量。
+
+// 写法一
+var tmp = newData;
+processData(tmp);
+storeData(tmp);
+
+// 写法二
+(function () {
+  var tmp = newData;
+  processData(tmp);
+  storeData(tmp);
+}());
+// 上面代码中，写法二比写法一更好，因为完全避免了污染全局变量。
+*/
+
+/**
+ * eval 命令：将字符串当作语句执行
+ * 
+ * eval没有自己的作用域，都在当前作用域内执行，因此可能会修改当前作用域的变量的值，造成安全问题。
+
+ * JavaScript 规定，如果使用严格模式，eval内部声明的变量，不会影响到外部作用域。
+
+ * 不建议使用
+ */
+
+
+
+
+
