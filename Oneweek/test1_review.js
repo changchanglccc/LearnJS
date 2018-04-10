e45045fe5ede4548ea71e1ca98eaffda30d0aa46
changchanglccc <lichong06091992@gmail.com>
2018-04-09 23:40:21 -0400

    review : Function + Operator

--- Oneweek/test1_review.js
+++ Oneweek/test1_review.js
@@ -45,19 +45,32 @@
 // console.log(f1()());    //没见到这么写的
 
 //------------------------------
-var jsonp = 'foo({"id": 42})';
+// var jsonp = 'foo({"id": 42})';
 
-var f = new Function('foo', jsonp);     //Function构造函数，与eval作用类似
-    // 相当于定义了如下函数
-    // function f(foo) {
-    //   foo({"id":42});
-    // }
-f(function(json){
-    console.log(json.id);
-})
+// var f = new Function('foo', jsonp);     //Function构造函数，与eval作用类似
+//     // 相当于定义了如下函数
+//     // function f(foo) {
+//     //   foo({"id":42});
+//     // }
+// f(function(json){
+//     console.log(json.id);
+// })
 
 // jsonp是一个字符串，Function构造函数将这个字符串，变成了函数体。调用该函数的时候，jsonp就会执行。
 // 这种写法的实质是将代码放到函数作用域执行，避免对全局作用域造成影响。
 // 不过，new Function()的写法也可以读写全局作用域，所以也是应该避免使用它。
 
+//------------------------------
+var obj = {p:1};
 
+var obj1 = {
+    valueOf: function(){
+        return 1;
+    }
+}
+console.log(obj.valueOf());
+console.log(obj.valueOf().toString());
+
+console.log(obj1);
+console.log(obj1.valueOf());
+// console.log(obj1.valueOf().toString());
